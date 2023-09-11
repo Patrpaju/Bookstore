aceac96828cbc27a40c821f8fc3329f9d87799e8
Patrick Pajula <patrick.pajula@gmail.com>
2023-09-11 12:46:00 +0300

    databases 1

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -4,10 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.Bookstore.model.Book;
 import com.example.Bookstore.model.BookRepository;
@@ -46,9 +44,4 @@ public class BookController {
 		model.addAttribute("book", repository.findById(BookId));
 		return "editbook";
 	}
-	@RequestMapping(value = "/edit/{id}", method = RequestMethod.POST)
-	public String edit(Book book) {
-		repository.save(book);
-		return "redirect:../bookstore";
-}
 }
